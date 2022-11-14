0c254efe70d46ff24b862996ebd770e0333bf792
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-11-14 16:31:28 -0500

    admin foundations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,7 +8,7 @@ from django.utils.text import slugify
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(1)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True) # Harry Potter 1 => harry-potter-1
@@ -16,10 +16,6 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
     
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
