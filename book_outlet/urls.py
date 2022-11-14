34d3326ea055e5b10c46de68d279e1c4888ddedd
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-11-14 15:43:13 -0500

    dummy project done

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("<slug:slug>", views.book_detail, name="book_detail")
 ]
